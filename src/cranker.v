8be803ac29de098508b428a78e26b17f8da43303
jacobh135 <hejacobj@icloud.com>
2026-07-19 13:21:59 -0500

    reimplemented and reverified optimal cranker

--- src/cranker.v
+++ src/cranker.v
@@ -1,11 +1,10 @@
 module cranker (
-    input wire clk, rst_n,
+    input wire c_done, clk, rst_n,
     input wire signed [17:0] c_x, c_y,
     output reg cranker_done,
-    output reg [4:0] out
+    output reg [4:0] count
 );
-    reg active;
-    reg [4:0] count;
+    reg [4:0] state;
     reg signed [17:0] z_x, z_y, c_x_s, c_y_s;
     wire signed [35:0] z_x_2, z_y_2, z_y_z_x;
 
@@ -15,53 +14,59 @@ module cranker (
 
     always @(posedge clk) begin
         if (~rst_n) begin
-            out <= 0;
-            count <= 0;
-            active <= 1;
+            state <= 0;
             z_x <= 0;
             z_y <= 0;
-            c_x_s <= c_x;
-            c_y_s <= c_y;
+            c_x_s <= 0;
+            c_y_s <= 0;
+            count <= 0;
             cranker_done <= 0;
         end
         else begin
-            case (active)
-                1: begin
-                    if (count == 24) begin
-                        out <= count;
-                        count <= 0;
-                        z_x <= 0;
-                        z_y <= 0;
+            case (state)
+                0: begin
+                    if (c_done) begin
+                        state <= state + 1;
+                        z_x <= z_x_2[32:15] - z_y_2[32:15] + c_x;
+                        z_y <= z_y_z_x[32:15] + z_y_z_x[32:15] + c_y;
                         c_x_s <= c_x;
                         c_y_s <= c_y;
-                        cranker_done <= 1;
-                    end
-                    else if (z_x_2 + z_y_2 > 37'sd4294967296) begin
-                        out <= count;
-                        count <= count + 1;
-                        active <= 0;
-                        cranker_done <= 0;
                     end
                     else begin
+                        state <= state;
+                    end
+                    count <= 0;
+                    cranker_done <= 0;
+                end
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22: begin
+                    if (z_x_2 + z_y_2 > 37'sd4294967296) begin
+                        state <= 0;
+                        z_x <= 0;
+                        z_y <= 0;
+                        c_x_s <= 0;
+                        c_y_s <= 0;
+                        count <= state;
+                        cranker_done <= 1;
+                    end
+                    else begin
+                        state <= state + 1;
                         z_x <= z_x_2[32:15] - z_y_2[32:15] + c_x_s;
                         z_y <= z_y_z_x[32:15] + z_y_z_x[32:15] + c_y_s;
-                        count <= count + 1;
-                        cranker_done <= 0;
                     end
                 end
-                0: begin
-                    if (count == 24) begin
-                        count <= 0;
-                        active <= 1;
-                        z_x <= 0;
-                        z_y <= 0;
-                        c_x_s <= c_x;
-                        c_y_s <= c_y;
-                        cranker_done <= 1;
+                23: begin
+                    if (z_x_2 + z_y_2 > 37'sd4294967296) begin
+                        count <= state;
                     end
                     else begin
-                        count <= count + 1;
+                        count <= state + 1;
                     end
+                    state <= 0;
+                    z_x <= 0;
+                    z_y <= 0;
+                    c_x_s <= 0;
+                    c_y_s <= 0;
+                    cranker_done <= 1;
                 end
             endcase
         end
--- test/test_cranker.py
+++ test/test_cranker.py
@@ -1,6 +1,11 @@
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import FallingEdge, ClockCycles
+from cocotb.triggers import FallingEdge
+
+async def reset_dut(dut):
+    dut.rst_n.value = 0
+    await FallingEdge(dut.clk)
+    dut.rst_n.value = 1
 
 def adjust(num, frac):
     num %= (2 ** (frac + 3))
@@ -9,11 +14,11 @@ def adjust(num, frac):
 
     return num
 
-def find_out(c_x, c_y, frac):
+def find_count(c_x, c_y, frac):
     z_x = 0
     z_y = 0
     count = 0
-    while ((z_x ** 2 + z_y ** 2 < 4 * (2 ** frac) ** 2) and (count < 24)):
+    while ((z_x ** 2 + z_y ** 2 <= 4 * (2 ** frac) ** 2) and (count < 24)):
         z_x_new = adjust((z_x ** 2) // (2 ** frac) - (z_y ** 2) // (2 ** frac) + c_x, frac)
         z_y_new = adjust(2 * ((z_x * z_y) // (2 ** frac)) + c_y, frac)
         z_x = z_x_new
@@ -24,63 +29,29 @@ def find_out(c_x, c_y, frac):
 @cocotb.test()
 async def test_cranker(dut):
     cocotb.start_soon(Clock(dut.clk, 10, unit="ns").start())
+    await reset_dut(dut)
 
     frac = 15
 
-    # load initial state
-    dut.rst_n.value = 0
-    dut.c_x.value = 0
-    dut.c_y.value = 0
-    await FallingEdge(dut.clk)
-    dut.rst_n.value = 1
-
-    # compute expected out
-    out = find_out(0, 0, frac)
-
-    # prepare next c inputs
-    dut.c_x.value = 1 * round(3.5 / 639 * (2 ** frac)) - round(2.5 * (2 ** frac))
-    dut.c_y.value = 0 * round(2 / 479 * (2 ** frac)) - 1 * (2 ** frac)
-
-    # receive inputs & advance
-    await FallingEdge(dut.clk)
-    count = 0
-
-    # run DUT
-    while (dut.cranker_done.value == 0):
-        await FallingEdge(dut.clk)
-        count += 1
-    
-    # check out
-    assert out == dut.out.value, f"Expected out={out}, got out={dut.out.value}"
-    assert count == 24, f"Expected count=24, got count={count}"
-
-    for i in range (1, 640 * 480):
-
-        # compute expected out
+    for i in range(640 * 480):
+        
         x_pos = i % 640
         y_pos = i // 640
 
         c_x = x_pos * round(3.5 / 639 * (2 ** frac)) - round(2.5 * (2 ** frac))
         c_y = y_pos * round(2 / 479 * (2 ** frac)) - 1 * (2 ** frac)
 
-        out = find_out(c_x, c_y, frac)
+        count = find_count(c_x, c_y, frac)
 
-        # prepare next c inputs
-        x_pos = (i + 1) % 640
-        y_pos = (i + 1) // 640
+        dut.c_x.value = c_x
+        dut.c_y.value = c_y
+        dut.c_done.value = 1
 
-        dut.c_x.value = x_pos * round(3.5 / 639 * (2 ** frac)) - round(2.5 * (2 ** frac))
-        dut.c_y.value = y_pos * round(2 / 479 * (2 ** frac)) - 1 * (2 ** frac)
-
-        # receive inputs & advance
         await FallingEdge(dut.clk)
-        count = 0
 
-        # run DUT
+        dut.c_done.value = 0
+
         while (dut.cranker_done.value == 0):
             await FallingEdge(dut.clk)
-            count += 1
-        
-        # check out
-        assert out == dut.out.value, f"Expected out={out}, got out={dut.out.value}"
-        assert count == 24, f"Expected count=24, got count={count}"
+
+        assert dut.count.value == count, f"Expected count={count}, got count={dut.count.value}"
